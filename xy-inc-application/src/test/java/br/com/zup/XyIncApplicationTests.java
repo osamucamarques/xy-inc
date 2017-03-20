78fc2d081142f823de4992e22cfaac3cff8a750c
samuelmarques <webmail.samuel@gmail.com>
2017-03-20 00:11:24 -0300

    Add support to run docker before tests

--- xy-inc-application/src/test/java/br/com/zup/XyIncApplicationTests.java
+++ xy-inc-application/src/test/java/br/com/zup/XyIncApplicationTests.java
@@ -6,6 +6,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.glassfish.jersey.client.HttpUrlConnectorProvider.ConnectionFactory;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.github.geowarin.junit.DockerRule;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -22,7 +25,7 @@ import br.com.zup.rest.api.InterestPointResource;
 import br.com.zup.vo.InterestPointVO;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = XyIncApplication.class)
 public class XyIncApplicationTests {
 
 	@Autowired
@@ -33,6 +36,17 @@ public class XyIncApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@ClassRule
+	public static DockerRule rabbitRule = DockerRule.builder()
+			.cmd("docker run -d --net=xy-inc-network --name=xy-inc-mysql -e MYSQL_ROOT_PASSWORD=xy-inc -e MYSQL_DATABASE=xy-inc -e MYSQL_USER=xy-inc -e MYSQL_PASSWORD=xy-inc mysql:5.6")
+			.waitForLog("Server startup complete").build();
+
+	@Test
+	public void testConnectsToDocker() throws Exception {
+		com.rabbitmq.client.ConnectionFactory factory = new com.rabbitmq.client.ConnectionFactory();
+		factory.newConnection(rabbitRule.getDockerHost(), rabbitRule.getHostPort("5672/tcp"));
+	}
 
 	@Test
 	public void contextLoads() {
